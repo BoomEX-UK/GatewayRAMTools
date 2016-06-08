5d140f9d3a666471a8d7e37d6a9d54ac5bc4962d
xJam-es <web@xjam.es>
2016-06-08 23:07:54 +0100

    More File Tidying

--- GatewayRAMTools/Forms/HeaderWindow.Designer.cs
+++ GatewayRAMTools/Forms/HeaderWindow.Designer.cs
@@ -28,34 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HeaderWindow));
             this.pnlButtons = new System.Windows.Forms.Panel();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnExport = new System.Windows.Forms.Button();
             this.pnlText = new System.Windows.Forms.Panel();
-            this.pnlTextLabels = new System.Windows.Forms.Panel();
-            this.pnlMainLabel = new System.Windows.Forms.Panel();
-            this.pnlMemregions = new System.Windows.Forms.Panel();
-            this.lblMain = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pnlTextLabels = new System.Windows.Forms.Panel();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.lblRAM = new System.Windows.Forms.Label();
             this.lblFile = new System.Windows.Forms.Label();
+            this.pnlMainLabel = new System.Windows.Forms.Panel();
+            this.lblMain = new System.Windows.Forms.Label();
+            this.pnlMemregions = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ramFrom = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ramTo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.filePosition = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dumpSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.pnlButtons.SuspendLayout();
             this.pnlText.SuspendLayout();
-            this.pnlTextLabels.SuspendLayout();
-            this.pnlMainLabel.SuspendLayout();
-            this.pnlMemregions.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.pnlTextLabels.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
+            this.pnlMainLabel.SuspendLayout();
+            this.pnlMemregions.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlButtons
@@ -99,53 +104,6 @@
             this.pnlText.Size = new System.Drawing.Size(284, 26);
             this.pnlText.TabIndex = 1;
             // 
-            // pnlTextLabels
-            // 
-            this.pnlTextLabels.Controls.Add(this.splitContainer2);
-            this.pnlTextLabels.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlTextLabels.Location = new System.Drawing.Point(0, 181);
-            this.pnlTextLabels.Name = "pnlTextLabels";
-            this.pnlTextLabels.Size = new System.Drawing.Size(284, 17);
-            this.pnlTextLabels.TabIndex = 2;
-            // 
-            // pnlMainLabel
-            // 
-            this.pnlMainLabel.Controls.Add(this.lblMain);
-            this.pnlMainLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlMainLabel.Location = new System.Drawing.Point(0, 0);
-            this.pnlMainLabel.Name = "pnlMainLabel";
-            this.pnlMainLabel.Size = new System.Drawing.Size(284, 23);
-            this.pnlMainLabel.TabIndex = 3;
-            // 
-            // pnlMemregions
-            // 
-            this.pnlMemregions.Controls.Add(this.listView1);
-            this.pnlMemregions.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlMemregions.Location = new System.Drawing.Point(0, 23);
-            this.pnlMemregions.Name = "pnlMemregions";
-            this.pnlMemregions.Padding = new System.Windows.Forms.Padding(6, 0, 6, 6);
-            this.pnlMemregions.Size = new System.Drawing.Size(284, 158);
-            this.pnlMemregions.TabIndex = 4;
-            // 
-            // lblMain
-            // 
-            this.lblMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblMain.Location = new System.Drawing.Point(0, 0);
-            this.lblMain.Name = "lblMain";
-            this.lblMain.Size = new System.Drawing.Size(284, 23);
-            this.lblMain.TabIndex = 0;
-            this.lblMain.Text = "{filename}";
-            this.lblMain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // listView1
-            // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.Location = new System.Drawing.Point(6, 0);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(272, 152);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -163,6 +121,14 @@
             this.splitContainer1.SplitterDistance = 136;
             this.splitContainer1.TabIndex = 1;
             // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 1;
+            // 
             // textBox2
             // 
             this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -171,13 +137,14 @@
             this.textBox2.Size = new System.Drawing.Size(132, 20);
             this.textBox2.TabIndex = 2;
             // 
-            // textBox1
+            // pnlTextLabels
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(136, 20);
-            this.textBox1.TabIndex = 1;
+            this.pnlTextLabels.Controls.Add(this.splitContainer2);
+            this.pnlTextLabels.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlTextLabels.Location = new System.Drawing.Point(0, 181);
+            this.pnlTextLabels.Name = "pnlTextLabels";
+            this.pnlTextLabels.Size = new System.Drawing.Size(284, 17);
+            this.pnlTextLabels.TabIndex = 2;
             // 
             // splitContainer2
             // 
@@ -216,6 +183,69 @@
             this.lblFile.Text = "File Offset";
             this.lblFile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // pnlMainLabel
+            // 
+            this.pnlMainLabel.Controls.Add(this.lblMain);
+            this.pnlMainLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMainLabel.Location = new System.Drawing.Point(0, 0);
+            this.pnlMainLabel.Name = "pnlMainLabel";
+            this.pnlMainLabel.Size = new System.Drawing.Size(284, 23);
+            this.pnlMainLabel.TabIndex = 3;
+            // 
+            // lblMain
+            // 
+            this.lblMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMain.Location = new System.Drawing.Point(0, 0);
+            this.lblMain.Name = "lblMain";
+            this.lblMain.Size = new System.Drawing.Size(284, 23);
+            this.lblMain.TabIndex = 0;
+            this.lblMain.Text = "{filename}";
+            this.lblMain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlMemregions
+            // 
+            this.pnlMemregions.Controls.Add(this.listView1);
+            this.pnlMemregions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMemregions.Location = new System.Drawing.Point(0, 23);
+            this.pnlMemregions.Name = "pnlMemregions";
+            this.pnlMemregions.Padding = new System.Windows.Forms.Padding(6, 0, 6, 6);
+            this.pnlMemregions.Size = new System.Drawing.Size(284, 158);
+            this.pnlMemregions.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ramFrom,
+            this.ramTo,
+            this.filePosition,
+            this.dumpSize});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(6, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(272, 152);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ramFrom
+            // 
+            this.ramFrom.Text = "RAM From";
+            // 
+            // ramTo
+            // 
+            this.ramTo.Text = "RAM To";
+            // 
+            // filePosition
+            // 
+            this.filePosition.Text = "File Position";
+            // 
+            // dumpSize
+            // 
+            this.dumpSize.Text = "Dump Size";
+            // 
             // HeaderWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -226,6 +256,7 @@
             this.Controls.Add(this.pnlTextLabels);
             this.Controls.Add(this.pnlText);
             this.Controls.Add(this.pnlButtons);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(300, 300);
             this.Name = "HeaderWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -233,19 +264,19 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HeaderWindow_FormClosed);
             this.pnlButtons.ResumeLayout(false);
             this.pnlText.ResumeLayout(false);
-            this.pnlTextLabels.ResumeLayout(false);
-            this.pnlMainLabel.ResumeLayout(false);
-            this.pnlMemregions.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.pnlTextLabels.ResumeLayout(false);
             this.splitContainer2.Panel1.ResumeLayout(false);
             this.splitContainer2.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
+            this.pnlMainLabel.ResumeLayout(false);
+            this.pnlMemregions.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -267,5 +298,9 @@
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.Label lblRAM;
         private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.ColumnHeader ramFrom;
+        private System.Windows.Forms.ColumnHeader ramTo;
+        private System.Windows.Forms.ColumnHeader filePosition;
+        private System.Windows.Forms.ColumnHeader dumpSize;
     }
 }
